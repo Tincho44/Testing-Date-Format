197227e528b1ce795297072f0042573927fcacf7
Tincho44 <martinharbuet@gmail.com>
2022-09-13 00:05:53 -0300

    Configuracion inicial

---
+++ tests/Library.Tests/DataFormatterTests.cs
@@ -0,0 +1,18 @@
+
+namespace Library.Tests;
+
+[TestFixture]
+public class DataFormatterTests
+{
+    [Test]
+    public void Setup()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
